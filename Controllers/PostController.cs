c6c4f135ddb2cdef4ef0500ae26ff8fd71ccd875
Cristina Camilo <89110319+cristinacamilolima@users.noreply.github.com>
2022-12-15 03:17:55 -0300

    chore: adiciona endpoint getall

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Project_for_Aceleration_Csharp_Tryitter.Context;
@@ -8,29 +7,26 @@ using Project_for_Aceleration_Csharp_Tryitter.Models;
 
 namespace Project_for_Aceleration_Csharp_Tryitter.Controllers
 {
-    public class PostController : Controller
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PostsController : ControllerBase
     {
-        [Route("[controller]")]
-        [ApiController]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public class PostsController : ControllerBase
+        private readonly AppDbContext _context;
+        public PostsController(AppDbContext context)
         {
-            private readonly AppDbContext _context;
-            public PostsController(AppDbContext context)
-            {
-                _context = context;
-            }
+            _context = context;
+        }
 
-            [HttpGet]
-            [AllowAnonymous]
-            public ActionResult<IEnumerable<Post>> GetAll()
-            {
-                var posts = _context.Posts!.AsNoTracking();
-                if (posts is null)
-                    return NotFound("Posts not found.");
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult<IEnumerable<Post>> GetAll()
+        {
+            var posts = _context.Posts!.AsNoTracking();
+            if (posts is null)
+                return NotFound("Posts not found.");
 
-                return Ok(posts);
-            }
+            return Ok(posts);
         }
     }
 }
